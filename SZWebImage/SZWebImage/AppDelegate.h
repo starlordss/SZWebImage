552bc50d5802928f010cd79ee5420bcb53427237
starlordss <zahi@Zahi.local>
2017-06-23 15:20:11 +0800

    项目准备

---
+++ SZWebImage/SZWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZWebImage
+//
+//  Created by Zahi on 2017/6/23.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SZWebImage/SZWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZWebImage
+//
+//  Created by Zahi on 2017/6/23.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
